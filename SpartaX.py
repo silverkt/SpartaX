4a754d63918b60e5aeaee78140b6fc3fa590793c
Silver.Sun <itisagift@yeah.net>
2017-05-17 18:00:07 +0800

    upd

--- SpartaX.py
+++ SpartaX.py
@@ -1,10 +1,69 @@
 #coding:utf-8
 import requests
 import re 
+import os
+
+def getCookie(url, headers=None):
+	return requests.get(url, headers= headers).cookies
+
+def getHtml(url, headers=None, cookies=None):
+	response = requests.get(url, headers= headers, cookies = cookies)
+	# cookie = response.cookies
+	# print(cookie)
+	# print(response.encoding)
+	response.encoding = 'utf-8'
+	# print(response.text)
+	return response.text
 
 
+
+def getProp(html, prop):
+	m = re.findall(('%s=.+?[\"|\'|>|\s]')%(prop), html , flags= re.I) 
+	index = 0
+	for x in m:
+
+		x = x.split('=')
+		m[index] = x[1].strip(' \"\'/>')
+		index = index + 1
+	return m
+
+	 
+
+def getAssets(src, name=None, headers=None, cookies=None, savePath=''):
+	response = requests.get(src, headers= headers, cookies=cookies)
+	src = src.split('/')
+	if not os.path.exists(savePath):
+		os.makedirs(savePath)
+	savePath = savePath + src[len(src)-1]
+	f = open(savePath, 'wb')
+	f.write(response.content)
+	f.close()
+
+
+def getContent(html, label):
+	m = re.findall(r'<%s>.+?</%s>'%(label,label), html, flags= re.I)
+	if len(m)!=0:
+		m = re.sub(r'<[^>]+>','', m[0])
+		return m
+	else:
+		return ''	
+
+
+	# for x in m:
+	# 	jpg_link = 'http://'+website+'/'+x;
+	# 	x = x.split('/')
+	# 	path = x[1]
+	# 	print(jpg_link)
+	# 	response = requests.get(jpg_link, headers= headers, cookies= cookie)
+	# 	data = response.content
+	# 	f = open(path,'wb')
+	# 	f.write(data)
+	# 	f.close()
+	# 	
+	# 	
 website = '91.t9n.space'
 url = "http://"+website+"/viewthread.php?tid=229471"
+prop = 'file'
 headers = {
 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 "Accept-Encoding":"utf-8",
@@ -14,42 +73,19 @@ headers = {
 "Referer":"http://www.google.com/",
 "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:32.0) Gecko/20100101 Firefox/32.0"
 }
-response = requests.get(url, headers= headers)
+cookie = getCookie(url, headers)
+html = getHtml(url, headers, cookie)
+x = getProp(html, prop)
+savePath = getContent(html, 'title')
+n = len(savePath)-45
+savePath = savePath[0:n]+'/'
 
-cookie = response.cookies 
- 
-print(response.encoding)
-response.encoding = 'utf-8'
-print(response.text)
-
-m = re.findall(r'file=".+?"', response.text)
-print(m[1])
-
-index = 0
-for x in m:
-
-	x = x.split('"')
-	m[index] = x[1]
-	#print(m[index])
-	index = index + 1
-
- 
+for i in x:
+	src =  'http://'+website+'/'+i;
+	print(src)
+	getAssets(src,headers= headers, cookies= cookie, savePath= savePath)
 
 
-
-
-for x in m:
-	jpg_link = 'http://'+website+'/'+x;
-	x = x.split('/')
-	path = x[1]
-	print(jpg_link)
-	response = requests.get(jpg_link, headers= headers, cookies= cookie)
-	data = response.content
-	f = open(path,'wb')
-	f.write(data)
-	f.close()
- 
- 
 # http://blog.csdn.net/sinat_21302587/article/details/65634124 
 # http://blog.csdn.net/shanzhizi/article/details/50903748
 # http://cn.python-requests.org/zh_CN/latest/user/quickstart.html#cookie
